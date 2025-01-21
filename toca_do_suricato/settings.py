011d4d66b3ceb2858a8e9d3851e529c9a4f10366
Angelion879 <diangelis879@outlook.com>
2025-01-21 20:13:34 -0300

    (update) set debug mode to False for deployment

--- toca_do_suricato/settings.py
+++ toca_do_suricato/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#qae9qjtj7&w@mkkvp!)y=!8zmqvzj_hvi8u(_bs%t36#ku-=@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost:8000','127.0.0.1:8000','suricamisas.rj.r.appspot.com']
 
